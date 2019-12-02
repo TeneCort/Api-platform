ac170d0b4ae982406e44082a80ef3798da8c47d6
Ioannis <ioannis.karapostolis@uha.fr>
2019-12-02 19:29:19 +0100

    Added file upload

---
+++ src/Entity/Book.php
@@ -0,0 +1,90 @@
+<?php
+// api/src/Entity/Book.php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * A book.
+ * @ApiResource(iri="http://schema.org/Book")
+ * @ORM\Entity
+ */
+class Book
+{
+    /**
+     * @var int The id of this book.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string|null The ISBN of this book (or null if doesn't have one).
+     *
+     * @ORM\Column(nullable=true)
+     */
+    public $isbn;
+
+    /**
+     * @var string The title of this book.
+     *
+     * @ORM\Column
+     */
+    public $title;
+
+    /**
+     * @var string The description of this book.
+     *
+     * @ORM\Column(type="text")
+     */
+    public $description;
+
+    /**
+     * @var string The author of this book.
+     *
+     * @ORM\Column
+     */
+    public $author;
+
+    /**
+     * @var \DateTimeInterface The publication date of this book.
+     *
+     * @ORM\Column(type="datetime")
+     */
+    public $publicationDate;
+
+    /**
+     * @var Review[] Available reviews for this book.
+     *
+     * @ORM\OneToMany(targetEntity="Review", mappedBy="book", cascade={"persist", "remove"})
+     * @ORM\Column(nullable=true)
+     */
+    public $reviews;
+
+    /**
+     * @var MediaObject|null
+     *
+     * @ORM\ManyToOne(targetEntity=MediaObject::class)
+     * @ORM\JoinColumn(nullable=true)
+     * @ApiProperty(iri="http://schema.org/image")
+     */
+    public $image;
+    
+    public function __construct()
+    {
+        $this->reviews = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
